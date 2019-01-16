2d5c611dfe499911d65c7ce7ee4daa24f39c858e
Vladimir Denisov <vdenisov.dev@gmail.com>
2019-01-16 16:56:50 +0300

    task4 is completed

--- tasks/task-4.js
+++ tasks/task-4.js
@@ -21,17 +21,51 @@ function getCatsNames(list) {
 }
 
 function getOldestMaleCats(list, n) {
-  const maleCats = list.filter(cat => cat.gender === 'male');
-  const maxAge = Math.max( ...maleCats.map(cat => cat.age) );
-  const oldestMaleCats = maleCats.filter(cat => cat.age === maxAge);
-  return oldestMaleCats.slice(0, n);
+  return list
+    .filter(cat => cat.gender === 'male')
+    .sort(sortBy('age', reverse = true))
+    .slice(0, n);
 }
 
 function getYoungestFemaleCats(list, n) {
-  const femaleCats = list.filter(cat => cat.gender === 'female');
-  const minAge = Math.min( ...femaleCats.map(cat => cat.age) );
-  const youngestMaleCats = femaleCats.filter(cat => cat.age === minAge);
-  return youngestMaleCats.slice(0, n);
+  return list
+    .filter(cat => cat.gender === 'female')
+    .sort(sortBy('age', reverse = false))
+    .slice(0, n);
+}
+
+// help function
+// => sort an array of objects by key with the ability to reverse it
+function sortBy(key, reverse) {
+  // copied from Stack Overflow:
+  // https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value#answer-23385176
+
+  /*
+    Move smaller items towards the front or back of the array
+    depending on if we want to sort the array in reverse order or not.
+  */
+  const moveSmaller = reverse ? 1 : -1;
+
+  /*
+    Move larger items towards the front or back of the array
+    depending on if we want to sort the array in reverse order or not.
+  */
+  const moveLarger = reverse ? -1 : 1;
+
+  return function (secondItem, firstItem) {
+    if (secondItem[key] < firstItem[key]) return moveSmaller;
+    if (secondItem[key] > firstItem[key]) return moveLarger;
+    return 0;
+  }
+}
+
+// help function
+// => pretty print of array and title for it
+function prettyPrint(title, data) {
+  console.log(`\n ##### ${title} ##### \n`);
+  for(let i=0; i<data.length; i++) {
+    console.log(JSON.stringify(data[i]));
+  }
 }
 
 // ######################### //
@@ -39,13 +73,6 @@ function getYoungestFemaleCats(list, n) {
 if (require.main === module) {
 
   const { catsGroupGenerate } = require('./task-3');
-
-  function prettyPrint(title, data) {
-    console.log(`\n ##### ${title} ##### \n`);
-    for(let i=0; i<data.length; i++) {
-      console.log(JSON.stringify(data[i]));
-    }
-  }
 
   // const list = catsGroupGenerate(999);
   let list = catsGroupGenerate(30);
@@ -55,12 +82,13 @@ if (require.main === module) {
   prettyPrint('Male cats', maleCats);
 
   let catsNames = getCatsNames(list);
-  prettyPrint('Cats` names', JSON.stringify(catsNames));
+  console.log(`\n ##### Cats' names ##### \n`);
+  console.log(JSON.stringify(catsNames));
 
-  let oldestMaleCats = getOldestMaleCats(list, 2);
+  let oldestMaleCats = getOldestMaleCats(list, 5);
   prettyPrint('Oldest male cats', oldestMaleCats);
 
-  let youngestFemaleCats = getYoungestFemaleCats(list, 2);
+  let youngestFemaleCats = getYoungestFemaleCats(list, 5);
   prettyPrint('Youngest female cats', youngestFemaleCats);
 
   console.log('');
